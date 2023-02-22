424b3b657d8ee0eaf96efaac44344d832bae6198
muhamadherwan <hello.wanlab.@gmail.com>
2023-02-22 13:37:21 +0800

    items data iterate with page break. But have bug, the last item dont display bottom border before page break:

--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -1,3 +1,8 @@
+{{-- @php
+    // $items = [1,2,3,4,5];
+    $items = [1,2];
+@endphp --}}
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -151,6 +156,11 @@
             padding-bottom: 10px;
         }
 
+        .items_section {
+            /* height: 30%;
+            background: rgb(245, 218, 210); */
+        }
+
         /* items table section */
         table.tb { 
             width:100%; 
@@ -178,6 +188,11 @@
         .page-break {
             page-break-after: always;
         }
+
+        .breakNow {
+            page-break-inside:avoid;
+            page-break-after:always;
+         }
 
     
     </style>
@@ -245,7 +260,6 @@
     </footer>
 
     <main>
-        
         <div id="contents">
         
             <table>
@@ -256,227 +270,59 @@
             </table>
             {{-- <div class="bold">Subject:&nbsp;<span class="subject">{{ $title }}</span></div> --}}
 
+            <div class="items_section">
             {{-- start items section --}}
             <table class="tb">
                 <tr>
-                  <th>No</th>
-                  <th>Description</th>
-                  <th>Quantity</th>
-                  <th>Unit Price (RM)</th>
-                  <th>SST 6%</th>
-                  <th>Total Amount (RM)</th>
+                    <th>No</th>
+                    <th>Description</th>
+                    <th>Quantity</th>
+                    <th>Unit Price (RM)</th>
+                    <th>SST 6%</th>
+                    <th>Total Amount (RM)</th>
                 </tr>
-                <tr>
-                  <td class="tcell">one</td>
-                  <td class="tcell">two</td>
-                  <td class="tcell">three</td>
-                  <td class="tcell">four</td>
-                  <td class="tcell">five</td>
-                  <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-                <tr>
-                    <td class="tcell">one</td>
-                    <td class="tcell">two</td>
-                    <td class="tcell">three</td>
-                    <td class="tcell">four</td>
-                    <td class="tcell">five</td>
-                    <td class="tcell">six</td>
-                </tr>
-              </table>
-            {{-- end items section --}}
-        
 
-        {{-- <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores cupiditate est modi aut atque maxime. Aspernatur, sapiente placeat libero aperiam ab facilis tempore ad porro id quo laudantium a rem voluptas explicabo voluptate, ratione cumque perspiciatis odit eveniet eaque ipsam sed obcaecati quasi et! Reprehenderit ullam consequuntur eveniet, sint minima optio expedita fuga sapiente exercitationem nam repellendus tempore cum consequatur itaque vitae esse totam eos praesentium labore quaerat vero accusamus sequi. Voluptates fugit dignissimos necessitatibus tenetur? Debitis facilis praesentium, temporibus rem delectus maiores? Voluptates velit necessitatibus quisquam, quo cupiditate eum assumenda voluptatem sed quos provident et quibusdam fugit dolorum fugiat? Quaerat maiores alias, asperiores eum obcaecati optio voluptates corporis accusamus. Illum aut ullam quasi earum consequuntur ratione dicta alias excepturi magnam sed, voluptatibus ut eius rerum aperiam ipsa inventore tenetur unde sunt assumenda laborum amet. Animi ipsa placeat, necessitatibus esse odit quia vel dolorum, repudiandae veritatis quidem numquam aliquam dicta non sequi illo ea beatae aspernatur saepe! Dicta autem dolore deserunt molestiae, commodi in incidunt, amet excepturi, similique quis cum aliquam magnam nisi dolor ut provident impedit nam esse nobis repudiandae enim tempora ipsa? Omnis at fugiat rem inventore repellat nihil eligendi, ea impedit suscipit dolor blanditiis nobis repudiandae et eveniet dolorem aut doloribus! Libero, officiis voluptates? Maiores ut placeat, fugiat ratione suscipit quod. Nisi sit accusamus minus ducimus doloremque dicta, vitae fuga, optio quasi iusto corrupti libero velit at! Accusantium rem veritatis nobis similique libero aliquid, consequatur veniam earum modi officiis, deserunt alias nam cum eligendi eaque quas? Iste atque perspiciatis iure, ut ea delectus nemo cum quas quaerat labore ducimus quod voluptates minima repellat vitae, optio molestias quidem ad quia facere. Ratione, porro beatae ut pariatur eius inventore vitae! Atque cumque veritatis iusto at beatae nemo sed, mollitia repellendus praesentium cum aspernatur voluptates? Sint temporibus sunt libero eum illo omnis expedita labore mollitia explicabo. Temporibus, facere illum. Quas reprehenderit non voluptates incidunt quaerat quasi at eos alias veritatis, ut nihil dolore temporibus laboriosam quis, tempore voluptas ducimus nostrum perspiciatis recusandae cum deserunt nam debitis magni id. Quam fugiat vel voluptatem fugit cumque amet eos dolores harum ratione cum! Minus laudantium beatae obcaecati, praesentium amet nesciunt consequuntur quibusdam, voluptates ipsum debitis, molestiae aliquid ullam tempora iste nisi adipisci eaque dolore error corrupti sed dolor placeat est. Impedit optio repellendus quo a, eius aperiam porro adipisci sint ea, hic ipsa dolor eligendi vitae veniam quibusdam aliquam tenetur nostrum! Cum aut laboriosam officia, odit a enim natus porro praesentium cupiditate delectus optio dolores aliquam, ipsum, et facere iusto? Amet et soluta facere in iusto minima, cupiditate provident temporibus quibusdam harum, repudiandae repellat. Facilis consequuntur sint, vel soluta a aliquid odio doloremque explicabo distinctio deleniti provident tenetur nobis! Voluptas, error culpa. Laboriosam placeat dicta optio ex perspiciatis molestias inventore quo eius tempora nam consectetur incidunt aliquam neque maxime odit totam laborum obcaecati eos, expedita quis nihil? Et dicta perferendis veniam totam quisquam culpa, modi tempora repellat. Beatae quia vero numquam, quidem autem voluptatum maxime molestiae pariatur esse debitis optio, maiores odio dignissimos!
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur doloremque velit repudiandae ratione. Amet, sunt illo! Fugit obcaecati harum alias tempora pariatur eveniet dolor accusamus, architecto laudantium soluta unde corrupti commodi repellat illum minus rem ad autem asperiores nisi iure? Ab tenetur ad blanditiis alias deleniti, dolorem ut sed, voluptas dicta incidunt ipsum sunt eum ea, illum nesciunt ipsam veniam consequuntur laborum. Excepturi laudantium quis repellat natus, pariatur ab magni hic maxime quae error. Adipisci ducimus recusandae quae doloribus quia sed in sequi dolore magni? Reiciendis soluta, quia pariatur praesentium quaerat at iste aspernatur, tempore veniam aliquid perspiciatis error excepturi molestiae est dolore sint corporis fugit cum nemo quo velit libero dolores cupiditate quasi? Maxime nisi pariatur nostrum, hic sed id nihil praesentium architecto nam minus enim iure culpa ex itaque nesciunt fugiat expedita optio illo nobis amet debitis temporibus accusamus vitae. Deserunt, ipsa incidunt qui alias quos nemo omnis voluptatum accusamus reprehenderit atque rerum nihil, tempore vero! Mollitia quidem reiciendis ea ad quia dolorum incidunt obcaecati culpa ut? Quam tempore eum perspiciatis praesentium reiciendis facilis repudiandae ab temporibus sit dignissimos? Dolore fuga maxime ullam facere quae dignissimos accusantium saepe exercitationem cumque perferendis debitis, officia obcaecati? Expedita, fuga nobis atque laborum dolor minima ratione a repellendus maxime esse accusamus, placeat debitis cum aliquid quaerat ad doloribus ab! Neque qui tempore at nostrum nihil laudantium dicta est iure, rerum saepe error ab, veniam unde provident illum explicabo delectus. Fugiat eos at sint explicabo vel eveniet quam a qui blanditiis quos, exercitationem quidem in. Saepe facilis error perferendis, dolore nisi consequatur. At in sed sequi amet inventore, iusto dolorum ipsa consectetur nulla! Eum neque modi, aliquid quae corrupti maiores aliquam beatae. Eum, odit officia distinctio delectus blanditiis porro animi quidem laudantium totam esse velit nobis ad minima, sequi fugiat! Iste officiis eveniet quas amet obcaecati dolores, doloribus ad quia sit quaerat maxime quasi exercitationem ducimus, inventore omnis ipsa aspernatur ullam. Dolores molestiae, sint, voluptatum dolorem iure quos sunt tenetur soluta voluptates aliquam aut provident quas accusamus illo tempore nobis, eius culpa quasi. Ex autem eum aperiam id dignissimos voluptates quae in ea alias assumenda. Magnam facere magni consequuntur debitis nam facilis aut nemo voluptate voluptas, consectetur a amet quos modi repudiandae neque? Quos, libero! Fugiat alias dolores ab iure repellendus enim, nihil nulla quibusdam quae iusto facere officia beatae quaerat minima consectetur a temporibus praesentium cupiditate. Facilis saepe provident facere molestiae similique nulla incidunt tenetur fuga, nihil dolores sapiente amet voluptates praesentium quis aliquid ducimus, animi aut quae? Ipsa obcaecati quas omnis, unde ipsum iste ratione et blanditiis commodi molestias autem aliquid sit illum consequatur inventore eos similique laudantium! Veritatis, tempore ullam doloribus dignissimos ratione neque. Magni culpa necessitatibus dicta a ipsa. Laboriosam perferendis nesciunt alias repellendus ipsa dolores modi reiciendis, accusamus mollitia libero ipsam eveniet obcaecati neque error? Mollitia illum quisquam maxime veniam aliquid harum doloremque, quasi magnam nulla tempora repellendus similique adipisci sit nesciunt quas ipsam voluptate saepe neque officia eaque possimus suscipit dignissimos necessitatibus. Quasi voluptatum eum accusamus quis?
-        </p> --}}
+                @php
+                    $n = 1;
+                    $items = [1];
+                    $items = [1,2];
+                    $items = [1,2,3];
+                    // $items = [1,2,3,4,5];
+                @endphp
+
+                @foreach ($items as $key=> $item)
+                    <tr>
+                        <td class="tcell">{{ $key + 1 }}</td>
+                        <td class="tcell">desc</td>
+                        <td class="tcell">three</td>
+                        <td class="tcell">four</td>
+                        <td class="tcell">five</td>
+                        <td class="tcell">{{ $key }}</td>
+                    </tr>
+                    <?php $n++ ?>
+
+                    
+                    @if ( $n % 3 == 0 )
+                        <div style="page-break-before:always"></div>
+                        
+                        
+                        {{-- <div style="page-break-inside:avoid; page-break-before:always"> </div> --}}
+                        {{-- <div class="breakNow"></div> --}}
+                            {{-- <div class="page-break"></div> --}}
+                    @endif
+                    
+                @endforeach      
+            </table>
+            {{-- end items section --}}
+            </div>
+            <br/><br/><br/><br/><br/>
+            <div class="signature_section">
+                <p>this is signature section</p>
+            </div>
+        
         
         </div> {{-- end content div --}}
         
-        {{-- <div class="page-break"></div>
-
-        <div id="contents">
-            <h1>This Page 2</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta perspiciatis error minus voluptatem possimus reprehenderit nihil modi dolor aspernatur reiciendis?</p>
-        </div> --}}
     </main>
 
 </body>
